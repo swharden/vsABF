625b0ea1d56c27fc913471928e5edc67f28db2c7
Scott W Harden <swharden@gmail.com>
2018-08-26 14:20:45 -0400

    project skeleton

---
+++ src/vsABF/ABF.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vsABF
+{
+    public class ABF
+    {
+        public ABF(string abfFilePath="./someFile.abf")
+        {
+            abfFilePath = System.IO.Path.GetFullPath(abfFilePath);
+            System.Console.WriteLine($"loading ABF File: {abfFilePath}");
+        }
+    }
+}
