b6bf60c886a23abc7158ce75174883efafcd7700
Scott W Harden <swharden@gmail.com>
2018-09-04 23:09:00 -0400

    started a concise ABFlib
    this class will be restricted to only reading specific variables from disk we find useful

---
+++ src/ABFLib/ABF.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+
+namespace ABFLib
+{
+
+    public class ABFsection
+    {
+        private const int BLOCKSIZE = 512;
+        public int block;
+        public int size;
+        public int count;
+        public int firstByte { get { return block * BLOCKSIZE; } }
+    }
+
+    public class ABF
+    {
+        private const int BLOCKSIZE = 512;
+
+        // ABF properties
+        public string abfFilePath;
+        public string abfID;
+        public int abfVersionMajor;
+        public int sweepCount;
+        public int channelCount;
+        public int sampleRate;
+
+        public int dataLengthSec;
+        public int dataPointCount;
+        public int dataFirstByte;
+        
+        // ABF2 sections
+        private ABFsection sectionProtocol;
+        private ABFsection sectionADC;
+        private ABFsection sectionDAC;
+        private ABFsection sectionEpoch;
+        private ABFsection sectionADCperDAC;
+        private ABFsection sectionEpochperDAC;
+        private ABFsection sectionStrings;
+        private ABFsection sectionData;
+        private ABFsection sectionTag;
+
+        // internal objects
+        private BinaryReader br;
+
+        /// <summary>
+        /// ABF1 and ABF2 file reader
+        /// </summary>
+        public ABF(string abfFilePath, bool preLoadData = true)
+        {
+            // format file paths
+            abfFilePath = Path.GetFullPath(abfFilePath);
+            this.abfFilePath = abfFilePath;
+            abfID = Path.GetFileNameWithoutExtension(abfFilePath);
+            
+            // determine if ABF1 or ABF2 file
+            FileOpen();
+            ReadFileFormat();
+            ReadSectionMap();
+            ReadSweepCount();
+            ReadChannelCount();
+            ReadSampleRate();
+            ReadDataInfo();
+
+            // release the file
+            FileClose();
+        }
+
+
+        /*
+         *
+         *
+         * 
+         * 
+         * ABF CLASS SUPPORTIVE FUNCTIONS
+         * 
+         * 
+         * 
+         * 
+        */
+
+        /// <summary>
+        /// Summarize this ABF's properties as a text block
+        /// </summary>
+        public string Info()
+        {
+            string info = $"### ABF Information for {abfID} ###\n";
+
+            // populate a dictionary with all fields in this class
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+            FieldInfo[] fields = this.GetType().GetFields(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Public);
+            foreach (FieldInfo x in fields)
+            {
+                System.Object val = x.GetValue(this);
+                string valStr = "null";
+
+                // if it's null, say it's null
+                if (val != null)
+                {
+                    valStr = val.ToString();
+                }
+
+                // format string based on object type
+                if (val is ABFsection)
+                {
+                    ABFsection section = (ABFsection)val;
+                    valStr = $"ABFsection (firstByte={section.firstByte}, size={section.size}, count={section.count})";
+                }
+                else if (val is BinaryReader)
+                {
+                    valStr = null;
+                }
+
+                // if our string contains text, show it
+                if (valStr != null)
+                    dict.Add(x.Name, valStr);
+            }
+
+            // print items in alphabetical order
+            List<string> keyList = new List<string>(dict.Keys);
+            keyList.Sort();
+            foreach (string key in keyList)
+            {
+                info += $"{key} = {dict[key]}\n";
+            }
+            return info+"\n";
+        }
+
+        /*
+         *
+         *
+         * 
+         * FILE OPERATIONS
+         * 
+         * 
+         * 
+        */
+
+        private void FileOpen()
+        {
+            if (File.Exists(abfFilePath))
+            {
+                br = new BinaryReader(File.Open(abfFilePath, FileMode.Open));       
+            } else
+            {
+                throw new Exception($"File does not exist: {abfFilePath}");
+            }
+        }
+
+        private void FileClose()
+        {
+            br.Close();
+        }
+
+        private double FileReadFloatSingle(int bytePosition = -1)
+        {
+            if (bytePosition >= 0)
+                br.BaseStream.Seek(bytePosition, SeekOrigin.Begin);
+            return (double)br.ReadSingle();
+        }
+
+        private double FileReadFloatDouble(int bytePosition = -1)
+        {
+            if (bytePosition >= 0)
+                br.BaseStream.Seek(bytePosition, SeekOrigin.Begin);
+            return (double)br.ReadDouble();
+        }
+
+        private int FileReadInt16(int bytePosition = -1)
+        {
+            if (bytePosition >= 0)
+                br.BaseStream.Seek(bytePosition, SeekOrigin.Begin);
+            return (int)br.ReadInt16();
+        }
+
+        private int FileReadInt32(int bytePosition = -1)
+        {
+            if (bytePosition >= 0)
+                br.BaseStream.Seek(bytePosition, SeekOrigin.Begin);
+            return (int)br.ReadInt32();
+        }
+
+        private int FileReadUInt16(int bytePosition = -1)
+        {
+            if (bytePosition >= 0)
+                br.BaseStream.Seek(bytePosition, SeekOrigin.Begin);
+            return (int)br.ReadUInt16();
+        }
+
+        private int FileReadUInt32(int bytePosition = -1)
+        {
+            if (bytePosition >= 0)
+                br.BaseStream.Seek(bytePosition, SeekOrigin.Begin);
+            return (int)br.ReadUInt32();
+        }
+
+        private string FileReadString(int charCount, int bytePosition = -1)
+        {
+            if (bytePosition >= 0)
+                br.BaseStream.Seek(bytePosition, SeekOrigin.Begin);
+            byte[] bytes = br.ReadBytes(charCount);
+            string txt = System.Text.Encoding.Default.GetString(bytes);
+            return txt;
+        }
+
+
+        /*
+         * 
+         * 
+         * 
+         * 
+         * READING OF VALUES FROM THE BINARY CONTENT OF ABF FILES
+         * 
+         * 
+         * 
+         * 
+         */
+
+        private void ReadFileFormat()
+        {
+            string firstFour = FileReadString(4, 0);
+            if (firstFour == "ABF ")
+                abfVersionMajor = 1;
+            else if (firstFour == "ABF2")
+                abfVersionMajor = 2;
+            else
+                throw new Exception($"Invalid ABF file: {abfFilePath}");
+        }
+
+        private void ReadSectionMap()
+        {
+            if (abfVersionMajor == 1)
+            {
+                // ABF1 files don't have a section map
+            }
+            else if (abfVersionMajor == 2)
+            {
+                sectionProtocol = ReadSection(76);
+                sectionADC = ReadSection(92);
+                sectionDAC = ReadSection(108);
+                sectionEpoch = ReadSection(124);
+                sectionADCperDAC = ReadSection(140);
+                sectionEpochperDAC = ReadSection(156);
+                sectionStrings = ReadSection(220);
+                sectionData = ReadSection(236);
+                sectionTag = ReadSection(252);
+            }
+        }
+
+        /// <summary>
+        /// Return the ABFsection for the section defined at firstByte
+        /// </summary>
+        private ABFsection ReadSection(int firstByte)
+        {
+            ABFsection section = new ABFsection();
+            section.block = FileReadUInt32(firstByte);
+            section.size = FileReadUInt32();
+            section.count = FileReadUInt32();
+            return section;
+        }
+
+        private void ReadSweepCount()
+        {
+            if (abfVersionMajor == 1)
+            {
+                int lActualEpisodes = FileReadUInt32(16);
+                sweepCount = lActualEpisodes;
+            }
+            else if (abfVersionMajor == 2)
+            {
+                int lActualEpisodes = FileReadUInt32(12);
+                sweepCount = lActualEpisodes;
+            }
+        }
+
+        private void ReadChannelCount()
+        {
+            if (abfVersionMajor == 1)
+            {
+                int nADCNumChannels = FileReadUInt16(120);
+                channelCount = nADCNumChannels;
+            }
+            else if (abfVersionMajor == 2)
+            {
+                channelCount = sectionADC.count;
+            }
+        }
+
+        private void ReadSampleRate()
+        {
+            if (abfVersionMajor == 1)
+            {
+                double fADCSampleInterval = FileReadFloatSingle(122);
+                sampleRate = (int)(1e6 / fADCSampleInterval);
+            }
+            else if (abfVersionMajor == 2)
+            {
+                double fADCSequenceInterval = FileReadFloatSingle(sectionProtocol.firstByte + 2);
+                sampleRate = (int)(1e6 / fADCSequenceInterval);
+            }
+        }
+
+        private void ReadDataInfo()
+        {
+            if (abfVersionMajor == 1)
+            {
+                int lDataSectionPtr = FileReadInt32(40);
+                dataFirstByte = lDataSectionPtr * BLOCKSIZE;
+
+                int lActualAcqLength = FileReadInt32(10);
+                dataPointCount = lActualAcqLength;
+            }
+            else if (abfVersionMajor == 2)
+            {
+                dataFirstByte = sectionData.firstByte;
+                dataPointCount = sectionData.count;
+            }
+
+            dataLengthSec = dataPointCount / sampleRate / channelCount;
+        }
+    }
+}
---
+++ src/ABFLibDemo/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace ABFLibDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(3, 43);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(794, 404);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.richTextBox1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.button1);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(794, 34);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "ABFLib Demo";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
+
