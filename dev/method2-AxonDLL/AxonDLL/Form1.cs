0d54d8f934326aeffeaebb01c35e50067f8e5bae
Scott W Harden <swharden@gmail.com>
2018-12-09 22:19:20 -0500

    make demo path-nonspecific

--- dev/method2-AxonDLL/AxonDLL/Form1.cs
+++ dev/method2-AxonDLL/AxonDLL/Form1.cs
@@ -13,17 +13,18 @@ namespace AxonDLL
         private void button1_Click(object sender, EventArgs e)
         {
             //string abfFilePath = @"C:\Users\scott\Documents\GitHub\pyABF\data\abfs\180415_aaron_temp.abf";
-            string abfFilePath = @"C:\Users\scott\Documents\GitHub\pyABF\data\abfs\171117_HFMixFRET.abf";
+            //string abfFilePath = @"C:\Users\scott\Documents\GitHub\pyABF\data\abfs\171117_HFMixFRET.abf";
             //string abfFilePath = @"C:\Users\scott\Documents\GitHub\pyABF\data\abfs\14o08011_ic_pair.abf";
             //string abfFilePath = @"C:\Users\scott\Documents\GitHub\pyABF\data\abfs\171116sh_0013.abf";
             //string abfFilePath = @"C:\Users\scott\Documents\GitHub\pyABF\data\abfs\16d22006_kim_gapfree.abf";
 
-            // https://github.com/swharden/pyABF/blob/master/data/headers/171116sh_0013.md
-
-            var abf = new AbfReader(abfFilePath);
-            richTextBox1.Text = abf.logString;
-            //richTextBox1.SelectionStart = richTextBox1.Text.Length;
-            //richTextBox1.ScrollToCaret();
+            OpenFileDialog diag = new OpenFileDialog();
+            diag.Filter = "ABF files (*.abf)|*.abf";
+            if (diag.ShowDialog() == DialogResult.OK)
+            {
+                var abf = new AbfReader(diag.FileName);
+                richTextBox1.Text = abf.logString;
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
