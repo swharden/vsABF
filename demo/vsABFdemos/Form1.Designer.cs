ac1564d59ffd1458f45516ba799ba12248693274
Scott W Harden <swharden@gmail.com>
2018-12-22 11:08:55 -0500

    update UC defaults

--- demo/vsABFdemos/Form1.Designer.cs
+++ demo/vsABFdemos/Form1.Designer.cs
@@ -28,13 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.userControl11 = new ABFInfoUC.UserControl1();
             this.SuspendLayout();
+            // 
+            // userControl11
+            // 
+            this.userControl11.Location = new System.Drawing.Point(69, 61);
+            this.userControl11.Name = "userControl11";
+            this.userControl11.Size = new System.Drawing.Size(282, 226);
+            this.userControl11.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(375, 223);
+            this.ClientSize = new System.Drawing.Size(489, 344);
+            this.Controls.Add(this.userControl11);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -43,6 +52,8 @@
         }
 
         #endregion
+
+        private ABFInfoUC.UserControl1 userControl11;
     }
 }
 
