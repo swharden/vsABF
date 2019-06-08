955f5882ced83ad8f050f1d25ea4b08f69e978be
Scott W Harden <swharden@gmail.com>
2019-06-08 16:53:54 -0400

    start a simple test suite

---
+++ tests/vsAbfTester/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vsAbfTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tester tester = new Tester();
+
+            string pyAbfDataFolder = @"C:\Users\scott\Documents\GitHub\pyABF\data\abfs";
+            if (System.IO.Directory.Exists(pyAbfDataFolder))
+                tester.testFolder(pyAbfDataFolder);
+
+            Console.WriteLine("\npress ENTER to exit...");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ tests/vsAbfTester/Tester.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vsAbfTester
+{
+    class Tester
+    {
+        public Tester()
+        {
+
+        }
+
+        public void testAbf(string abfFilePath)
+        {
+            var abf = new vsABF.ABF(abfFilePath);
+            Console.WriteLine(abf);
+            abf.Close();
+        }
+
+        public void testFolder(string abfFolderPath)
+        {
+            foreach (string abfFilePath in System.IO.Directory.GetFiles(abfFolderPath, "*.abf"))
+                testAbf(abfFilePath);
+        }
+    }
+}
