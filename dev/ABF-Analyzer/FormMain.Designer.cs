38107014c5f94f115a88e152af4355eadaa4a3ab
Scott W Harden <swharden@gmail.com>
2019-06-07 20:10:11 -0400

    discrete triangle lowpass filter

--- dev/ABF-Analyzer/FormMain.Designer.cs
+++ dev/ABF-Analyzer/FormMain.Designer.cs
@@ -31,8 +31,14 @@
             this.scottPlotUC1 = new ScottPlot.ScottPlotUC();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.btnAnalyzeSweep = new System.Windows.Forms.Button();
+            this.cbFilter = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.nudFilterMs = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
             this.detectionParamatersUC1 = new ABF_Analyzer.DetectionParamatersUC();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudFilterMs)).BeginInit();
             this.SuspendLayout();
             // 
             // scottPlotUC1
@@ -52,7 +58,7 @@
             this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.Location = new System.Drawing.Point(12, 298);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(230, 141);
+            this.groupBox1.Size = new System.Drawing.Size(230, 60);
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Analyze";
@@ -67,6 +73,63 @@
             this.btnAnalyzeSweep.UseVisualStyleBackColor = true;
             this.btnAnalyzeSweep.Click += new System.EventHandler(this.BtnAnalyzeSweep_Click);
             // 
+            // cbFilter
+            // 
+            this.cbFilter.AutoSize = true;
+            this.cbFilter.Location = new System.Drawing.Point(6, 24);
+            this.cbFilter.Name = "cbFilter";
+            this.cbFilter.Size = new System.Drawing.Size(74, 21);
+            this.cbFilter.TabIndex = 1;
+            this.cbFilter.Text = "enabled";
+            this.cbFilter.UseVisualStyleBackColor = true;
+            this.cbFilter.CheckedChanged += new System.EventHandler(this.CbFilter_CheckedChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.nudFilterMs);
+            this.groupBox2.Controls.Add(this.cbFilter);
+            this.groupBox2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(12, 364);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(230, 60);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Filter";
+            // 
+            // nudFilterMs
+            // 
+            this.nudFilterMs.DecimalPlaces = 1;
+            this.nudFilterMs.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.nudFilterMs.Location = new System.Drawing.Point(86, 23);
+            this.nudFilterMs.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.nudFilterMs.Name = "nudFilterMs";
+            this.nudFilterMs.Size = new System.Drawing.Size(74, 25);
+            this.nudFilterMs.TabIndex = 2;
+            this.nudFilterMs.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            65536});
+            this.nudFilterMs.ValueChanged += new System.EventHandler(this.NudFilterMs_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(166, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ms";
+            // 
             // detectionParamatersUC1
             // 
             this.detectionParamatersUC1.Location = new System.Drawing.Point(12, 12);
@@ -79,6 +142,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1093, 451);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.detectionParamatersUC1);
             this.Controls.Add(this.scottPlotUC1);
@@ -86,6 +150,9 @@
             this.Text = "ABF Analyzer";
             this.Load += new System.EventHandler(this.FormMain_Load);
             this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudFilterMs)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -96,6 +163,10 @@
         private DetectionParamatersUC detectionParamatersUC1;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button btnAnalyzeSweep;
+        private System.Windows.Forms.CheckBox cbFilter;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown nudFilterMs;
     }
 }
 
--- dev/ABF-Analyzer/FormMain.cs
+++ dev/ABF-Analyzer/FormMain.cs
@@ -33,11 +33,12 @@ namespace ABF_Analyzer
         {
             abf = new vsABF.ABF(abfFileName);
             scottPlotUC1.plt.Clear();
-            scottPlotUC1.plt.PlotSignal(abf.sweepY, abf.sampleRate, markerSize: 0);
+            scottPlotUC1.plt.PlotSignal(abf.sweepY, abf.sampleRate, markerSize: 2);
             scottPlotUC1.plt.Title(abf.abfFilename);
             scottPlotUC1.plt.YLabel($"Channel {abf.sweepChannel} (pA)");
             scottPlotUC1.plt.XLabel($"Time (milliseconds)");
             scottPlotUC1.plt.AxisAuto(0, .1);
+            scottPlotUC1.plt.Axis(7.0, 7.75, -100, 50);
             scottPlotUC1.Render();
         }
 
@@ -61,6 +62,71 @@ namespace ABF_Analyzer
             }
 
             scottPlotUC1.Render();
+        }
+
+        private void ApplyFilterToSweep()
+        {
+            System.Diagnostics.Stopwatch stopwatch = System.Diagnostics.Stopwatch.StartNew();
+
+            // start by re-loading the current sweep to ensure it's fresh
+            abf.SetSweep(abf.sweepNumber, abf.sweepChannel);
+            if ((cbFilter.Checked == false) || (nudFilterMs.Value <= 0))
+            {
+                scottPlotUC1.Render();
+                return;
+            }
+
+
+            // set aside memory to do this
+            double[] smooth = new double[abf.sweepY.Length];
+
+            /// create the kernal to be a shape you want
+
+            // create a triangle kernel
+            int triangleRampPoints = (int)(20 * (double)nudFilterMs.Value); // I know there are 20 points per msec
+            double[] kernel = new double[triangleRampPoints * 2 - 1];
+            for (int i = 0; i < triangleRampPoints; i++)
+                kernel[i] = 1 + i;
+            for (int i = triangleRampPoints; i < kernel.Length; i++)
+                kernel[i] = kernel.Length - i;
+            Console.WriteLine($"Triangle kernel size: {kernel.Length} points");
+
+            // normalize the kernel so its sum is 1
+            double kernelSum = 0;
+            for (int i = 0; i < kernel.Length; i++)
+                kernelSum += kernel[i];
+            for (int i = 0; i < kernel.Length; i++)
+                kernel[i] /= kernelSum;
+
+            // apply the kernel manually (this would be faster with a deconvolution)
+            for (int i = kernel.Length; i < abf.sweepY.Length - kernel.Length; i++)
+            {
+                smooth[i] = 0;
+                for (int j = 0; j < kernel.Length; j++)
+                    smooth[i] += kernel[j] * abf.sweepY[i + j];
+            }
+            // clean up the ends
+            for (int i = 0; i < kernel.Length; i++)
+            {
+                smooth[i] = smooth[kernel.Length+1];
+                smooth[smooth.Length - i - 1] = smooth[smooth.Length - kernel.Length - 2];
+            }
+            Array.Copy(smooth, abf.sweepY, smooth.Length);
+
+            double elapsedMsec = stopwatch.ElapsedTicks * 1000.0 / System.Diagnostics.Stopwatch.Frequency;
+            System.Console.WriteLine(string.Format("filter applied in {0:0.00} ms", elapsedMsec));
+
+            scottPlotUC1.Render();
+        }
+
+        private void CbFilter_CheckedChanged(object sender, EventArgs e)
+        {
+            ApplyFilterToSweep();
+        }
+
+        private void NudFilterMs_ValueChanged(object sender, EventArgs e)
+        {
+            ApplyFilterToSweep();
         }
     }
 }
